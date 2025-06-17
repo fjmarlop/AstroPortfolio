280e86f38c7ff81969f21b8b4cc6102c190df06b
Francisco Javier Marmolejo <fjmarlop@hotmail.com>
2025-06-18 00:03:39 +0200

    add nmap y ctf

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -14,32 +14,15 @@ window.cargarContenido = async function (categoria, slug) {
 
     const data = await res.json();
 
-    // ahora se carga el HTML externo
     const htmlRes = await fetch(data.html);
     if (!htmlRes.ok) throw new Error('No se pudo cargar el HTML');
 
     const html = await htmlRes.text();
 
-    contenidoEl.innerHTML = `
-      <h1 class="text-white text-2xl mb-4 text-center">${data.title}</h1>
-       <section class="w-full md:w-2/3 mx-auto bg-neutral-900 bg-opacity-50 rounded-lg flex flex-col md:flex-row p-4">
-       
-        <div class="hidden md:block">
-          <img src="/img/${data.slug}/${data.slug}.png" class="rounded-lg p-4" alt="Imagen CTF">
-        </div>
+    const cabeceraHTML = generarCabecera(categoria, data);
 
-        <div class="flex flex-col justify-center text-green-500 md:ml-6 text-lg">
-          <span class="p-2 md:p-3">
-            Plataforma:
-            <a href="${data.enlacePlataforma}" target="_blank"
-              class="hover:text-lime-400 no-underline">
-              ${data.plataforma}
-            </a>
-          </span>
-          <span class="p-2 md:p-3">Dificultad: ${data.dificultad}</span>
-          <span class="p-2 md:p-3">Fecha de resolución: ${data.fecha}</span>
-        </div>
-      </section>
+    contenidoEl.innerHTML = `
+      ${cabeceraHTML}
       ${html}
     `;
   } catch (e) {
@@ -47,6 +30,45 @@ window.cargarContenido = async function (categoria, slug) {
     console.error(e);
   }
 };
+
+function generarCabecera(categoria, data) {
+  switch (categoria) {
+    case 'ctfs':
+      return `
+        <h1 class="text-white text-2xl mb-4 text-center">${data.title}</h1>
+        <section class="w-full md:w-2/3 mx-auto bg-neutral-900 bg-opacity-50 rounded-lg flex flex-col md:flex-row p-4">
+          <div class="hidden md:block">
+            <img src="/img/${data.slug}/${data.slug}.png" class="rounded-lg p-4 object-contain"
+              style="max-width: 200px; max-height: 200px;" alt="Imagen CTF">
+          </div>
+          <div class="flex flex-col justify-center text-green-500 md:ml-6 text-lg">
+            <span class="p-2 md:p-3">
+              Plataforma:
+              <a href="${data.enlacePlataforma}" target="_blank"
+                class="hover:text-lime-400 no-underline">
+                ${data.plataforma}
+              </a>
+            </span>
+            <span class="p-2 md:p-3">Dificultad: ${data.dificultad}</span>
+            <span class="p-2 md:p-3">Fecha de resolución: ${data.fecha}</span>
+          </div>
+        </section>
+      `;
+
+    case 'tools':
+      return `
+        <h1 class="text-2xl mb-4 text-center">${data.title}</h1>
+      `;
+
+    case 'tecnicas':
+      return `
+        <h1 class="text-2xl font-bold mb-4 text-center">${data.title}</h1>
+      `;
+
+    default:
+      return `<h1 class="text-white text-2xl mb-4 text-center">${data.title}</h1>`;
+  }
+}
 
 
 
